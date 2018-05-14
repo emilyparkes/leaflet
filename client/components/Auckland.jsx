46476e5df21056f4768af21c17ca6141800c8ed7
Emily Parkes <emilycoco@me.com>
2018-05-15 11:57:19 +1200

    marker pop up

--- client/components/Auckland.jsx
+++ client/components/Auckland.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 class Auckland extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      position: [
+        -36.8670921833,
+        174.7585096333
+      ],
+      zoom: 13
+    }
+  }
   render() {
-    const position = [-36.8670921833, 174.7585096333]
-    const zoom = 15
-    const url = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
-    const attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
-    const accessToken = 'pk.eyJ1IjoianVsaWEtbWFyZWlrZSIsImEiOiJjamdhY2VyNHoxMXFyMnlwam05dWphd3Y4In0.yAxAhojYaX8Q4Xcf-Qh2JQ'
 
     return (
-      <Map center={position} zoom={zoom}>
+      <Map center={this.state.position} zoom={this.state.zoom}>
         <TileLayer
-          id={'mapbox.pencil'}
-          url={url}
-          attribution={attribution}
-          accessToken={accessToken}
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1pbHljb2NvIiwiYSI6ImNqaDZ2bzRtYTAxaGEyeXA3NHV3em00c3YifQ.vPrXAwEUUzaJ27sV39ATqw"
         />
+        <Marker position={this.state.position}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
       </Map>
     )
   }
