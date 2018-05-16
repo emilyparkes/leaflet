f0648676452aeb5fb85a5507a191dbb2c0cc74eb
Emily Parkes <emilycoco@me.com>
2018-05-16 16:55:35 +1200

    finish test data switch to try it in event-app

--- server/db/data.js
+++ server/db/data.js
@@ -1,18 +1,19 @@
-{
+[
   {
-    address: '304 West Tamaki Road, Glendowie, Auckland'
-    position: [
+    eventname: 'Farmers Market',
+    address: '304 West Tamaki Road, Glendowie, Auckland',
+    coords: [
       -36.869751,
       174.879524
     ]
-  }
-
+  },
   {
-    address: '65 Aeroview Drive, Beach Haven, Auckland'
-    position: [
+    eventname: 'Flower Store Opening',
+    address: '65 Aeroview Drive, Beach Haven, Auckland',
+    coords: [
       -36.794702,
       174.685521
     ]
   }
 
-}
+]
